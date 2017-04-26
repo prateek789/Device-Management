c7f0677450d8c58834d73fe8b1bd105dbaca8cfc
Prateek <prateek.jain1@ideavate.com>
2017-04-26 14:57:56 +0530

    add user erorr

--- myapp/views.py
+++ myapp/views.py
@@ -21,4 +21,8 @@ def page_not_found(request):
     return render(request, '404.html', status=404)
 
 def server_error(request):
-    return render(request, '500.html', status=500)   
+    return render(request, '500.html', status=500)
+
+
+def user_error(request):
+    return render(request, '500.html', status=500)
