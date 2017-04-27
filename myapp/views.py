c7c22aa045868cc7d21bfd469cde1486c08322a7
Prateek <prateek.jain1@ideavate.com>
2017-04-27 14:01:22 +0530

    add admin6 errr

--- myapp/views.py
+++ myapp/views.py
@@ -49,3 +49,6 @@ def admin4_error(request):
 def admin5_error(request):
     return render(request, '500.html', status=500)
 
+def admin6_error(request):
+    return render(request, '500.html', status=500)
+
